e0f4d0c7f35cd358054aed10190f451081e2e73d
Alexaverse <anushkaa22kumari@gmail.com>
2025-07-06 21:51:39 +0530

    done

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback,useEffect } from 'react'
+import { useState, useCallback,useEffect,useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setnumberAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(true);
   const [password, setPassword] = useState("")
+
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -18,6 +20,12 @@ function App() {
     }
     setPassword(pass)
   }, [length, numberAllowed, charAllowed, setPassword])
+
+  const copyPassword=useCallback(()=>{
+    passwordRef.current?.select()
+    // to select upon particular length passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password)
+  },[password])
   
   useEffect(()=>{passwordGenerator()},[length, numberAllowed, charAllowed,passwordGenerator])
 
@@ -26,8 +34,8 @@ function App() {
       <div className='w-full max-w-md h-full py-3 mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
         <h1 className='text-white text-center '>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden my-3'>                  
-          <input type="text" value={password} className='outline-none rounded w-full py-1 px-3 mx-2' placeholder='password' readOnly />
-          <button className='outline-none bg-blue-700 text-white px-3 shrink-0'>copy</button>
+          <input type="text" value={password} className='outline-none rounded w-full py-1 px-3 mx-2' placeholder='password' readOnly ref={passwordRef} />
+          <button onClick={copyPassword} className='outline-none bg-blue-700 text-white px-3 shrink-0'>copy</button>
         </div>
         <div className='flex text-sm gap-x-2 '>
           <div className='flex items-center gap-x-1'>
